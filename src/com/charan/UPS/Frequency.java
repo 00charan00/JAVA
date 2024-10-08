69b8b6ccf797a7f157608d33f3e7f0a301a80b7e
00charan00 <poycharan@gmail.com>
2024-10-08 09:01:54 +0530

    8/10.1

---
+++ src/com/charan/UPS/Frequency.java
@@ -0,0 +1,23 @@
+package com.charan.UPS;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Frequency {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 2, 3, 1, 4, 4, 4, 5};
+        int n = arr.length;
+
+        Map<Integer, Integer> mp = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            if (mp.containsKey(arr[i])) {
+                mp.put(arr[i], mp.get(arr[i]) + 1);
+            } else {
+                mp.put(arr[i], 1);
+            }
+        }
+        for (Map.Entry<Integer, Integer> entry : mp.entrySet()) {
+            System.out.println(entry.getKey() + "--->" + entry.getValue());
+        }
+    }
+}
